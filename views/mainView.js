346e87f99bbc28d953a339bbd6fd2a4013f6db9f
uakaminoe <noe.villegas333@gmail.com>
2014-03-20 21:56:43 -0600

    Inicio de la creción de Home

--- views/mainView.js
+++ views/mainView.js
@@ -1,39 +1,23 @@
-/*define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'text!/templates/main.html'
-], function($, _, Backbone, mainTemplate){
-  var ProjectListView = Backbone.View.extend({
-    el: $("#container"),
-    initialize: function(){
-      // Compile the template using Underscores micro-templating
-      var compiledTemplate = _.template( mainTemplate );
-      this.$el.html(compiledTemplate);
-    }
-  });
-  // Returning instantiated views can be quite useful for having "state"
-  return ProjectListView;
-});*/
 define([
     "jquery",
     "underscore",
     "backbone",
     'text!/templates/home'
-    ], function( $, _, Backbone, mainTemplate ) {
+    ], function( $, _, Backbone, mainTemplate) {
 
     var mainView = Backbone.View.extend({
         el: 'body',
         initialize:function(){
-            this.render();
+           // this.render();
         },
         events:{
             "click #mainButton":"actionButon"
         },
         render: function(){
-            var holaMundo={mainvar:"now", mainButton:"Press Me"};
-            var template= _.template( mainTemplate,holaMundo );
-            this.$el.html(template);
+            var view=this;
+            var variables={mainvar:"now", mainButton:"Press Me"};
+            var maintemplate= _.template( mainTemplate,variables);
+            this.$el.html(maintemplate);
         },
         actionButon:function(){
             alert('hihi');
